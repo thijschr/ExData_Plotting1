6e83c4af5de3ec90ab44254787b531c6f0818edf
thijschr <thijs.vanson@runbox.com>
2014-05-11 12:59:26 +0200

    Added data prep file

---
+++ R/dataPrep.R
@@ -0,0 +1,38 @@
+## Data preparation
+
+
+# Loading data ------------------------------------------------------------
+
+## url link to data
+fileurl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+
+## Downloading data
+download.file(fileurl, 
+              destfile="household_power_consumption.zip", 
+              method="curl")
+
+## Reading data
+powerConsumption <- read.table(unz("household_power_consumption.zip", 
+                                   "household_power_consumption.txt"), 
+                               header=T, 
+                               sep=";",
+                               na.strings = '?')
+
+powerConsumption <- na.omit(powerConsumption)
+
+
+# Data conversion ---------------------------------------------------------
+
+powerConsumption <- transform(powerConsumption, Date = as.Date(Date, '%d/%m/%Y'))
+
+
+# Extract Dates of interest and write to csv file -------------------------
+
+twoDays <- subset(powerConsumption, Date == '2007-02-01' | Date == '2007-02-02')
+
+
+
+
+## Other solution to the date time conversion
+# object$DateTime <- strptime(paste(object$Date, object$Time), format="%d/%m/%Y %H:%M:%S")
+# finalData$datetime <- as.POSIXlt(paste(finalData$Date,finalData$Time))
